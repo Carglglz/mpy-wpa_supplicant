42a93daed01c49e50af222335142b27652cf5b12
Carlosgg <carlosgilglez@gmail.com>
2022-12-08 02:30:43 +0000

    add notify for verbose output
        & print error if wpa_supplicant.config is not present

--- wpa_supplicant.py
+++ wpa_supplicant.py
@@ -3,13 +3,15 @@ import network
 from machine import unique_id
 from binascii import hexlify
 import time
+import sys
+
 try:
     from hostname import NAME
 except Exception:
-    NAME = 'upydevice_{}'.format(hexlify(unique_id()))
+    NAME = f"{sys.platform}-{hexlify(unique_id())}"
 
 
-def setup_network(timeout=10):
+def setup_network(timeout=10, notify=True):
     n = 0
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -19,20 +21,26 @@ def setup_network(timeout=10):
     scan_tuples.sort(key=lambda x: x[1], reverse=True)
     # Set device hostname
     wlan.config(dhcp_hostname=NAME)
-    with open('wpa_supplicant.config', 'r') as wpa_conf:
-        wifi_config = json.load(wpa_conf)
+    try:
+        with open("wpa_supplicant.config", "r") as wpa_conf:
+            wifi_config = json.load(wpa_conf)
+    except Exception as e:
+        sys.print_exception(e, sys.stdout)
+        return
     _APs_in_range = [x[0] for x in scan_tuples if x[0] in wifi_config.keys()]
     if _APs_in_range:
         _ssid = _APs_in_range[0]
         if not wlan.isconnected():
-            print('Connecting to network...')
+            if notify:
+                print("Connecting to network...")
             wlan.connect(_ssid, wifi_config[_ssid])
             while not wlan.isconnected():
                 n += 1
                 time.sleep(1)
                 if n > timeout:
                     return False
-        print('Connected to {}'.format(_ssid))
-        print('Network Config:', wlan.ifconfig())
+        if notify:
+            print(f"Connected to {_ssid}")
+            print("Network Config:", wlan.ifconfig())
 
         return True
