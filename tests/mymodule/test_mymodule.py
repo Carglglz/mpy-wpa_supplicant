0cc97e538ce0d93fa0c77e372f3a5e112b029d19
Carlosgg <carlosgilglez@gmail.com>
2023-03-17 20:41:19 +0000

    test package update

--- tests/mymodule/test_mymodule.py
+++ tests/mymodule/test_mymodule.py
@@ -9,6 +9,9 @@ class TestSum(unittest.TestCase):
     def test_sum_tuple(self):
         self.assertEqual(sum((mymodule.ret(), 2, 1)), 6, "Should be 6")
 
+    def test_diff_tuple(self):
+        self.assertEqual(mymodule.ret() - 2, 1, "Should be 1")
+
 
 if __name__ == "__main__":
     unittest.main()
